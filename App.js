0d068a7d7e0a808cd387865d0f3d22fcc89f8125
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2023-05-12 17:02:46 -0300

    adicionando controladores para erro

--- App.js
+++ App.js
@@ -1,54 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import * as Location from 'expo-location'
 import { useEffect, useState } from 'react';
 
 export default function App() {
   const [location , setLocation] = useState({})
   const [address, setAddress] = useState("")
-  const [latitude, setLatitude] = useState()
-  const [longitude, setLongitude] = useState()
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
   const [addressTWo , setAddressTwo ] = useState()
 
+try{
+  useEffect(()=>{
+    const getPermission = async()=>{
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if(status !== 'granted'){
+        console.log('Please grant location permission');
+        return;
+      }
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      setAddressTwo(currentLocation)
+      console.log('location');
+      console.log(currentLocation);
+    };
+    getPermission();
+  },[location]);
+}catch(err){
+  Alert.alert(`erro ${err}`)
+}
     useEffect(()=>{
-      const getPermission = async()=>{
-        let {status} = await Location.requestForegroundPermissionsAsync();
-        if(status !== 'granted'){
-          console.log('Please grant location permission');
-          return;
-        }
-        let currentLocation = await Location.getCurrentPositionAsync({});
-        setLocation(currentLocation);
-        setAddressTwo(currentLocation)
-        console.log('location');
-        console.log(currentLocation);
-      };
-      getPermission();
-    },[]);
+      console.log('latitude: ',latitude);
+      console.log('longitude: ',longitude);
+    },[latitude,longitude])
 
     const geocode = async()=>{
-     if(address === ""){
-                setLatitude(location.coords.latitude);
-                setLongitude(location.coords.longitude);
-                console.log('>>>>>>>>>>>>>>>>>> location atual >>>>>>>>>>>>>>>>>>>>')
-                console.log('latitude atual: ',latitude);
-                console.log('longitude atual: ',longitude);
-            console.log("     ");
-           }else{
-                const geocodedLocation = await Location.geocodeAsync(address);
-                console.log('>>>>>>>>>>>>', address)
-                console.log('>>>>>>>>>>>>>>>>>> location >>>>>>>>>>>>>>>>>>>>');
-                setLatitude(geocodedLocation[0].latitude);
-                setLongitude(geocodedLocation[0].longitude);
-                console.log('latitude: ',latitude);
-                console.log('longitude: ',longitude);
-                console.log("     ");
+    try{
+        if(address === ""){
+                   setLatitude(location.coords.latitude);
+                   setLongitude(location.coords.longitude);
+                   console.log('>>>>>>>>>>>>>>>>>> location atual >>>>>>>>>>>>>>>>>>>>')
+               console.log("     ");
+              }else{
+                   const geocodedLocation = await Location.geocodeAsync(address);
+                   console.log('>>>>>>>>>>>>', address)
+                   console.log('>>>>>>>>>>>>>>>>>> location >>>>>>>>>>>>>>>>>>>>');
+                   setLatitude(geocodedLocation[0].latitude);
+                   setLongitude(geocodedLocation[0].longitude);
+                   console.log("     ");
+             }
+            }catch(err){
+              Alert.alert(`error 404: ${err}`)
+            }
           }
-    }
 
 
   return (
     <View style={styles.container}>
+      <Text>{"latitude: "+latitude}</Text>
+      <Text>{"longitude: "+longitude}</Text>
       <TextInput placeholder = 'Address' value={address} onChangeText={setAddress}  required={true}/>
       <Button title="Geocode Address" onPress={geocode}/>
       <StatusBar style="auto" />
